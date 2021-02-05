8859719c953a5b13117d6f612133698147dff8fe
Sarvesh Kulkarni <sarveshgk10@gmail.com>
2021-02-05 19:03:02 +0530

    CleverTap Intern Project

--- app/src/main/java/com/example/interns/MainActivity.java
+++ app/src/main/java/com/example/interns/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import com.clevertap.android.sdk.CleverTapAPI;
 
+import java.util.Date;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         profileUpdate.put("Email", "sarveshgk10@gmail.com");
 //Update custom profile properties
         profileUpdate.put("Plan Type", "Platinum");
-       // profileUpdate.put("Identity", 61026032);
         profileUpdate.put("Phone", "+919819569167");
         profileUpdate.put("Gender", "Male");
 
 
-        clevertapDefaultInstance.pushProfile(profileUpdate);
-        clevertapDefaultInstance.pushEvent("Product viewed");
+        //clevertapDefaultInstance.pushProfile(profileUpdate);
+        //clevertapDefaultInstance.pushEvent("Product viewed");
 
+        HashMap<String, Object> prodViewedAction = new HashMap<String, Object>();
+        prodViewedAction.put("Product Name", "Nike Shoes");
+        prodViewedAction.put("Category", "Shoes");
+        prodViewedAction.put("Price", 159.99);
+        prodViewedAction.put("Date", new Date());
+
+        clevertapDefaultInstance.pushEvent("Product viewed", prodViewedAction);
 //        CleverTapAPI cleverTapAPI = CleverTapAPI.getDefaultInstance(getApplicationContext());
 //        cleverTapAPI.createNotificationChannel(getApplicationContext(),"54321","Sarvesh","CleverTap notification", NotificationManager.IMPORTANCE_MAX,true);
 //
